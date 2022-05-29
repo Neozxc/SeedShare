f8c114d15e833ed13c71f716c11bef38d72b98a2
Nedas <neduska991@gmail.com>
2022-05-29 22:14:13 +0100

    Fix typo

--- front-end/src/utils/data.js
+++ front-end/src/utils/data.js
@@ -1,7 +1,7 @@
 // Fetch sanity data from specific file
 export const userQuery = (userId) => {
     // Try to get me document of type equal to user and under score id is equal to user id
-    const query = `*[_type == "user" && _id == '${userId}]`;
+    const query = `*[_type == "user" && _id == '${userId}']`;
 
     return query;
 }
