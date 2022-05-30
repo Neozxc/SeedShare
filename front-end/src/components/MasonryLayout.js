626ae5021b2e5def8f88cc05cbf75d52d5a7245b
Nedas <neduska991@gmail.com>
2022-05-30 23:58:44 +0100

    Image typo fixed ?? >.> :D

--- front-end/src/components/MasonryLayout.js
+++ front-end/src/components/MasonryLayout.js
@@ -15,10 +15,9 @@ const MasonryLayout = ({ pins }) => {
   }
 
   return (
-    <div>
-      <Masonry className='flex animate-slide-fwd' breakpointCols={breakpointObj} />
-      {pins?.map((pin) => <Pin key={pin._id} pin={pin} className="w-max" />)}
-    </div>
+      <Masonry className='flex animate-slide-fwd' breakpointCols={breakpointObj}>
+        {pins?.map((pin) => <Pin key={pin._id} pin={pin} className="w-max" />)}
+      </Masonry>
   )
 }
 
