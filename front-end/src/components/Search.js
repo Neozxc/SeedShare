d2d3cecfcd0a578562f2f8b49399b402ce7c61ff
Nedas <neduska991@gmail.com>
2022-05-30 23:52:34 +0100

    Search stuff done

--- front-end/src/components/Search.js
+++ front-end/src/components/Search.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-const Search = () => {
+import Spinner from './Spinner';
+import { feedQuery, searchQuery } from '../utils/data';
+import { client } from '../client';
+import MasonryLayout from './MasonryLayout';
+
+const Search = ({ searchTerm }) => {
+  const [pins, setPins] = useState(null);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    if (searchTerm) {
+      const query = searchQuery(searchTerm.toLowerCase());
+      setLoading(true);
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      })
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      })
+    }
+  }, [searchTerm]);
+
   return (
-    <div>Search</div>
+    <div>
+      {loading && <Spinner message="Searching for pins..." />}
+      {pins?.length !== 0 && <MasonryLayout pins={pins} />}
+      {pins?.length === 0 && searchTerm !== '' && !loading && (
+        <div className='mt-10 text-center text-xl'>No pins found mate</div>
+      )}
+    </div>
   )
 }
 
