76f31334516aac8e28feb9c618b0cf18f4ba5130
Nedas <neduska991@gmail.com>
2022-06-01 00:29:51 +0100

    Regenerate token

--- front-end/src/client.js
+++ front-end/src/client.js
@@ -3,12 +3,12 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient ({
-    projectId: 'ib2sfeub',
+    projectId: `${process.env.REACT_APP_SANITY_PROJECT_ID}`,
     dataset: 'production',
     apiVersion: '2022-05-29',
     // Show more quickly images for people around the world
     useCdn: true,
-    token: "sk7uonGCFLCZAy2LIy7eqoXiwi3oUq1bCeYDLce8hn7g8KJMyOhg3MzM9jmIuyfvD8K9s2QfLchYsdlAgAP8iMe7oMXRvcYoXkZj0vndUc8ZpsJUQXaCeB3iWZ45OYI0rXJoT0CJUTeIkmnQG2AUVc4U3liBzEVN6X5mGVPsD8oEcNP7OrWP"
+    token: `${process.env.REACT_APP_SANITY_TOKEN}`
 });
 
 // Make builder for our images
