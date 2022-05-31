d6e1b098749ea6bceed93dd0a076ffd64f4caaa3
Nedas <neduska991@gmail.com>
2022-06-01 00:34:09 +0100

    .env - doesn't work in this case

--- front-end/src/client.js
+++ front-end/src/client.js
@@ -3,12 +3,12 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient ({
-    projectId: `${process.env.REACT_APP_SANITY_PROJECT_ID}`,
+    projectId: `ib2sfeub`,
     dataset: 'production',
     apiVersion: '2022-05-29',
     // Show more quickly images for people around the world
     useCdn: true,
-    token: `${process.env.REACT_APP_SANITY_TOKEN}`
+    token: `skNx1jSrPrLwtrstcCilrjTNRgLFio5w2vxMHzpgoFjMOtcJqh0hjXsiILmI4pdEsUQcydngcF8zZ1Lmp90y5eG1fUGO2a1tK0kESZI59ldgdVNrS28mjTJjMlyTtUxISt6SprhBgw3d1LcI78lbJypV92mzCeNTXxSt4rbVmXszyCEB3DHV`
 });
 
 // Make builder for our images
