5b181340733eb9663c6fce9eb3ad6075480e8ae6
Nedas <neduska991@gmail.com>
2022-06-01 00:24:39 +0100

    .

--- front-end/src/client.js
+++ front-end/src/client.js
@@ -8,7 +8,7 @@ export const client = sanityClient ({
     apiVersion: '2022-05-29',
     // Show more quickly images for people around the world
     useCdn: true,
-    token: `${process.env.REACT_APP_SANITY_TOKEN}`
+    token: "sk7uonGCFLCZAy2LIy7eqoXiwi3oUq1bCeYDLce8hn7g8KJMyOhg3MzM9jmIuyfvD8K9s2QfLchYsdlAgAP8iMe7oMXRvcYoXkZj0vndUc8ZpsJUQXaCeB3iWZ45OYI0rXJoT0CJUTeIkmnQG2AUVc4U3liBzEVN6X5mGVPsD8oEcNP7OrWP"
 });
 
 // Make builder for our images
